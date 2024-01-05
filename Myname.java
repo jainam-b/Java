c4a3982a919f9db9a1445dcbd5f39ad2edb2dfb0
jainam-b <jainambagrecha16@gmail.com>
2024-01-05 11:58:54 +0530

    program updated

--- Myname.java
+++ Myname.java
@@ -1,12 +1,12 @@
-// Create a class name as my name which has 3 variable which shows date month and year 
-// write three constructor default ,copy and parameterize  and a method print date in format DD-MM-YYYY
-class Myname {
+// // Create a class name as my name which has 3 variable which shows date month and year 
+// // write three constructor default ,copy and parameterize  and a method print date in format DD-MM-YYYY
+// class Myname {
 
-    public static void main(String[] args) {
-        int dd = 16;
-        int mm = 10;
-        int yy = 2003;
-        System.out.println(dd+"/"+mm+"/"+yy);
-    }
+//     public static void main(String[] args) {
+//         int dd = 16;
+//         int mm = 10;
+//         int yy = 2003;
+//         System.out.println(dd+"/"+mm+"/"+yy);
+//     }
 
-}
+// }
